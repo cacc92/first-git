a4085e16e51a9edbc87e46d5ac99e60423c9bd3b
Cesar <ccarrascocarre@gmail.com>
2025-04-21 10:27:01 -0400

    Modifico la clase para utilizar Lombok

--- src/main/java/com/ccarrrasco/first/git/models/Usuario.java
+++ src/main/java/com/ccarrrasco/first/git/models/Usuario.java
@@ -1,24 +1,16 @@
 package com.ccarrrasco.first.git.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "usuarios")
+@Getter @Setter @ToString
+@AllArgsConstructor @NoArgsConstructor
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Usuario(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
