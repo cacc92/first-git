4a63ac7f79acb0703ed767582163d9e85fa7071c
duoc.Ricky <cliner7800@gmail.com>
2025-04-21 10:20:37 -0400

    Primary key Usuario

--- src/main/java/com/ccarrrasco/first/git/models/Usuario.java
+++ src/main/java/com/ccarrrasco/first/git/models/Usuario.java
@@ -1,4 +1,17 @@
 package com.ccarrrasco.first.git.models;
 
 public class Usuario {
+    private Long id;
+
+    public Usuario(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
